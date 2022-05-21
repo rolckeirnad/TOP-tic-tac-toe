8335e69da978bbce02cc6ac0936d66cc0bbdf784
Daniel Gallardo <dgallardo.c@outlook.com>
2022-05-21 14:04:43 -0500

    Add logic to display a winner

--- js/app.js
+++ js/app.js
@@ -13,27 +13,41 @@ const gameBoard = (function () {
         }
     })();
 
+    // Function to mark selected cell
     function addMarker(index) {
-        if (!board[index]) { // Check if cell is empty
-            const turn = gameController.turn.getTurn();
+        const isCellEmpty = !board[index];
+        if (isCellEmpty) {
+            const turn = gameController.turn.getTurn(); // Get which player is going to play
             const mark = gameController.players[turn].getMark(); // Get mark for current player
-            board[index] = mark; // Save mark in array
+            board[index] = mark; // Save mark in array position
             cells[index].textContent = mark; // Show mark in pressed cell
-            gameController.turn.toggleTurn(); // Toggle actual turn
+            gameController.endTurn(index, turn); // Finish player turn
         }
     }
+    return {
+        board,
+    };
 })();
 
 const gameController = (function () {
-    // 
+    // Factory for creating players
     function Player(name, mark) {
+        let cells = [];
         const getName = () => name;
         const getMark = () => mark;
+        const addCell = (index) => cells.push(index);
+        const getCells = () => cells;
         return {
             getName,
             getMark,
+            addCell,
+            getCells,
         }
     };
+
+    // Cached DOM elements
+    const messageContainer = document.querySelector('.message');
+    const message = document.querySelector('#messageText');
 
     // Internal state for controller
     const turn = (function () {
@@ -47,13 +61,53 @@ const gameController = (function () {
             getTurn: getActualTurn,
             toggleTurn: toggleActualTurn,
         }
-        
+
     })();
 
+    // All possible winning combinations
+    const POSSIBLE_WINNING_COMBINATIONS = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    // Finalizes turn and check if there's winner or not
+    const endTurn = function (playedIndex, player) {
+        players[player].addCell(playedIndex);// Save index cell in player cells list
+        checkWinner(player);// Check played markers
+        turn.toggleTurn(); // Toggle actual turn
+    };
+
+    // We check if player has all indexes of a winning combination
+    const checkWinner = function (player) {
+        const playerCells = [...players[player].getCells()];
+        let fullBoard = !(gameBoard.board.includes(undefined));
+
+        const winner = POSSIBLE_WINNING_COMBINATIONS.some(combination =>
+            combination.every(index => playerCells.includes(index))
+        );
+
+        if (winner) { // There's a winner
+            displayResult(`${players[player].getName()} wins!!`);
+        } else if (fullBoard) { // If there isn't a winner and the board is full
+            displayResult('It\'s a tie!!!');
+        }
+    };
+
+    const displayResult = function (result) {
+        messageContainer.classList.add('show');
+        message.textContent = result;
+    };
+
     const players = [Player('Player 1', 'X'), Player('Player 2', 'O')];
-    console.log("Here goes the rest of the game...", `${players[0].getName()} VS ${players[1].getName()}`);
     return {
         players,
         turn,
+        endTurn,
     }
 })();
